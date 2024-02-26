476cadc6bc6824914b306a4bc7d9acda6cd287e8
dfsmarsi <douglassmarsi10@gmail.com>
2024-02-26 17:06:50 -0300

    upando

---
+++ SigecomTestesUI/Config/DriverFabrica.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using System;
+using System.Diagnostics;
+
+namespace SigecomTesteUI.Config
+{
+    public class DriverFabrica
+    {
+        public const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
+        public const string AppId = @"C:\SIGECOM\SIGECOM.exe";
+
+        public WindowsDriver<WindowsElement> CriarDriver()
+        {
+            WindowsDriver<WindowsElement> driver;
+
+            AbrirWinAppDriver();
+
+            AppiumOptions appOptions = new AppiumOptions();
+            appOptions.AddAdditionalCapability("app", AppId);
+            driver = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), appOptions);
+            Assert.NotNull(driver);
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+
+
+            return driver;
+        }
+
+        public void AbrirWinAppDriver()
+        {
+            string WinAppDriver = @"C:\Program Files (x86)\Windows Application Driver\WinAppDriver.exe";
+            Process.Start(WinAppDriver);
+        }
+    }
+}
---
+++ SigecomTestesUI/Services/DriverService.cs
@@ -0,0 +1,157 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Interactions;
+using SigecomTesteUI.Config;
+using System;
+using System.Threading;
+
+namespace SigecomTestesUI.Services
+{
+    public class DriverService : IDisposable
+    {
+        private readonly WindowsDriver<WindowsElement> _driver;
+
+        public DriverService()
+        {
+            _driver = new DriverFabrica().CriarDriver();
+        }
+
+        public void FecharSistema()
+        {
+            ClicarBotaoName("Sair/Login");
+            //ClicarBotaoName(", Sim (ENTER)");
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            TrocarJanela();
+            ValidarElementoExistentePorNome("Sistema de gestão comercial");
+            ClicarBotaoName("Fechar");
+        }
+
+        public void ValidarElementoExistentePorNome(string valor)
+        {
+            var elemento = EncontrarElementoName(valor);
+            Assert.AreEqual(elemento.Text, valor);
+        }
+
+        public WindowsElement EncontrarElementoId(string idElemento)
+        {
+            return _driver.FindElementByAccessibilityId(idElemento);
+        }
+
+        public WindowsElement EncontrarElementoName(string nomeElemento)
+        {
+            return _driver.FindElementByName(nomeElemento);
+        }
+
+        public WindowsElement EncontrarElementoXPath(string caminhoElemento)
+        {
+            return _driver.FindElementByXPath(caminhoElemento);
+        }
+
+        public string ObterValorElementoId(string nomeElemento)
+        {
+            var elemento = _driver.FindElementByAccessibilityId(nomeElemento);
+            return elemento.Text;
+        }
+
+        public string ObterValorElementoName(string nomeElemento)
+        {
+            var elemento = _driver.FindElementByName(nomeElemento);
+            return elemento.Text;
+        }
+
+        public string PegarValorDaColunaDaGrid(string nomeColuna)
+        {
+           return _driver.FindElementByName($"{nomeColuna} row 0").Text;
+        }
+
+        public void TrocarJanela()
+        {
+            // Identify the current window handle. You can check through inspect.exe which window this is.
+            var currentWindowHandle = _driver.CurrentWindowHandle;
+            // Wait for 5 seconds or however long it is needed for the right window to appear/for the splash screen to be dismissed
+            Thread.Sleep(TimeSpan.FromSeconds(3));
+            // Return all window handles associated with this process/application.
+            // At this point hopefully you have one to pick from. Otherwise you can
+            // simply iterate through them to identify the one you want.
+            var allWindowHandles = _driver.WindowHandles;
+            // Assuming you only have only one window entry in allWindowHandles and it is in fact the correct one,
+            // switch the session to that window as follows. You can repeat this logic with any top window with the same
+            // process id (any entry of allWindowHandles)
+            _driver.SwitchTo().Window(allWindowHandles[0]);
+        }
+
+        public void DigitarNoCampoId(string idElemento, string texto)
+        {
+            var elemento = _driver.FindElementByAccessibilityId(idElemento);
+            elemento.SendKeys(texto);
+        }
+
+        public void DigitarNoCampoName(string nomeElemento, string texto)
+        {
+            var elemento = _driver.FindElementByName(nomeElemento);
+            elemento.SendKeys(texto);
+        }
+
+        public void DigitarNoCampoEnterId(string idElemento, string texto)
+        {
+            var elemento = _driver.FindElementByAccessibilityId(idElemento);
+            elemento.SendKeys(texto);
+            elemento.SendKeys(Keys.Enter);
+        }
+
+        public void DigitarNoCampoEnterName(string nomeElemento, string texto)
+        {
+            var elemento = _driver.FindElementByAccessibilityId(nomeElemento);
+            elemento.SendKeys(texto);
+            elemento.SendKeys(Keys.Enter);
+        }
+
+        public void ClicarBotaoName(string nomeBotao)
+        {
+            var botao = _driver.FindElementByName(nomeBotao);
+            botao.Click();
+        }
+
+        public void ClicarBotaoId(string nomeBotao)
+        {
+            var botao = _driver.FindElementByAccessibilityId(nomeBotao);
+            botao.Click();
+        }
+
+        public void DarDuploCliqueNoBotaoName(string nomeBotao)
+        {
+            var botao = _driver.FindElementByName(nomeBotao);
+            Actions acao = new Actions(_driver);
+            acao.MoveToElement(botao);
+            acao.DoubleClick();
+            acao.Perform();
+        }
+
+        public void SelecionarItemComboBox(string nomeCampo, int posicao)
+        {
+            var campo = _driver.FindElementByAccessibilityId(nomeCampo);
+            campo.Click();
+            EncontrarElementoNaComboBox(posicao, campo);
+            campo.SendKeys(Keys.Enter);
+        }
+
+        private static void EncontrarElementoNaComboBox(int posicao, WindowsElement campo)
+        {
+            for (int i = 1; i <= posicao; i++)
+                campo.SendKeys(Keys.ArrowDown);
+        }
+
+        public void FecharJanelaComEsc(string nomeJanela)
+        {
+            var janela = _driver.FindElementByName(nomeJanela);
+            janela.SendKeys(Keys.Escape);
+        }
+
+        public void Dispose()
+        {
+            FecharSistema();
+            _driver.Quit();
+        }
+    }
+}
